ee1cac7f1698e8587ec306d486347f13b91ab0e2
dgiltz <giltzdeclan@gmail.com>
2023-03-27 22:02:55 -0400

    Reformatting

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,13 +1,24 @@
 import numpy as np
-from numpy import random
-import pandas as pd
 
-class NaiveBayes():
-    def fit(self, X, y, type : str = "Gaussian"):
-        match(type.capitalize):
-            case "GAUSSIAN","GAUSS","NORMAL","NORM","G","N":
+
+class NaiveBayes:
+    def fit(self, X, y, type: str = "Gaussian"):
+        match (type.capitalize):
+            case "GAUSSIAN", "GAUSS", "NORMAL", "NORM", "G", "N":
                 pass
-            case "BERNOULLI","BERN","B":
+            case "BERNOULLI", "BERN", "B":
                 pass
             case "MULTINOMIAL", "MULTI", "MULT", "M":
                 pass
+
+    def train(self, X, y):
+        pass
+
+    def predict():
+        pass
+
+    def _gaussian():
+        pass
+
+    def _bernoulli():
+        pass
